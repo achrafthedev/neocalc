48f9924ba788f519104963b3dab243b1dd6a5e5f
achrafthedev <achrafchardoudi2002@gmail.com>
2026-06-09 21:52:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path for GitHub Pages: https://achrafthedev.github.io/react-calculator-app/
-  base: '/react-calculator-app/',
+  // Base path for GitHub Pages: https://achrafthedev.github.io/neocalc/
+  base: '/neocalc/',
   server: {
     port: 3000,
     open: true
